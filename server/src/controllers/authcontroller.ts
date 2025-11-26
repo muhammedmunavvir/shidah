1801accf4594fb8231d3f2bf874b1af567d4dee1
muhammedmunavvir <munavvirmuhammed296@gmail.com>
2025-11-26 19:00:55 +0530

    add domain on cookie set backend railway url

--- server/src/controllers/authcontroller.ts
+++ server/src/controllers/authcontroller.ts
@@ -58,7 +58,7 @@ export const googleLogin = async (req: Request, res: Response) => {
       httpOnly: true,
       secure: isProd,
       sameSite: isProd ? "none" : "lax",
-      domain: ".shidah.vercel.app",
+      domain: ".up.railway.app",
       path: "/",
       maxAge: 60 * 1000,
     });
