45dc8eb9503b85404e1045209ef2db9e0ce37eaa
muhammedmunavvir <munavvirmuhammed296@gmail.com>
2025-11-26 19:29:43 +0530

    create versel file and change base url

--- client/src/services/api.tsx
+++ client/src/services/api.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { toast } from "sonner";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+baseURL: "/api",
   withCredentials: true,
 });
 
