751330d7a9eff4e5e7a901bb065dc20dab748bf7
muhammedmunavvir <munavvirmuhammed296@gmail.com>
2025-11-26 19:03:32 +0530

    api.tsx error log

--- client/src/services/api.tsx
+++ client/src/services/api.tsx
@@ -14,6 +14,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
+      console.log(error.response,"error.response")
       toast.error("Your session has expired. Please log in again.");
       // JWT expired or invalid
       const { logout } = useAuthStore.getState();
